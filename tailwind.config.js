ad0481936984ca17437cb2c5462fda5866e776d2
hengage <tunecarterkillz@gmail.com>
2022-08-26 01:35:32 +0100

    Styles: custom css in twilwind
    color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './objectives.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#2d3191" ,
+        "secondary": "#e78e26",
+        "textcolor1": "#3b3333",
+        "textcolor2": "#606060",
+      }
+    },
   },
   plugins: [],
 }
